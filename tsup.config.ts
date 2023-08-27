e8680e3b7c623baee17118f81c639041ddecd92f
otto <ottofy@zohomail.com>
2023-08-27 12:49:57 -0600

    1.0.1

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entryPoints: ['src/index.ts'],
+  entryPoints: {
+    monthpicker: 'src/index.ts',
+  },
   format: ['cjs', 'esm'],
   dts: true,
   sourcemap: false,
-  clean:true,
-  splitting: true,
+  clean: true,
   minify: true,
   target: 'es2018',
   tsconfig: 'tsconfig.json',
   external: ['tslib'],
-})
+});
