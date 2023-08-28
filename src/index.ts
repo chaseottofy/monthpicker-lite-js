a7ea4d0446b8b262799d579d13717c0a61a1b63e
otto <ottofy@zohomail.com>
2023-08-28 15:09:04 -0600

    update version

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-// declare const MonthPicker: {
-//     new (rootContainer?: HTMLElement, startDate?: Date, pickerCallbacks?: import("./models/interfaces").DatepickerCallback, theme?: string, format?: string, closeOnSelect?: boolean, onlyShowCurrentMonth?: boolean, alignPickerMiddle?: boolean): MonthPickerInterface;
-//     prototype: MonthPickerInterface;
-// };
 import './index.css';
 export { default as MonthPicker } from './monthpicker/monthpickerClass';
 export * from './models/interfaces';
