8faf954768ab2e69229ec815d53ad1efc804a1d2
otto <ottofy@zohomail.com>
2023-08-29 02:53:59 -0600

    create date manip tests

--- src/helpers/dateHelpers.ts
+++ src/helpers/dateHelpers.ts
@@ -8,11 +8,21 @@ const {
   DEFAULT_FORMAT
 } = pickerConstants as PickerConstantsInterface;
 
-function isLeapYear(year: number): boolean {
+/**
+ * isLeapYear
+ * @param year number - Year to test
+ * @returns boolean - True if leap year
+ */
+export function isLeapYear(year: number): boolean {
   return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 }
 
-function getOrdinal(num: number) {
+/**
+ * getOrdinal
+ * @param num - day of month
+ * @returns string - day of month with ordinal (e.g. '1st', '2nd', '3rd', '4th', etc.)
+ */
+export function getOrdinal(num: number) {
   const int = Math.round(num);
   if (Number.isNaN(int)) return null;
   const digits = [int % 10, int % 100];
@@ -24,27 +34,67 @@ function getOrdinal(num: number) {
     : int + ordinals[3];
 }
 
+/**
+ * isDateValid - Must be valid date object with a
+ * positive valueOf (within 60 or so years)
+ * 
+ * @param val Date to validate
+ * @returns boolean
+ */
 export function isDateValid(val: Date) {
-  return !Number.isNaN(new Date(val).valueOf());
+  const tstNum: number = new Date(val).valueOf();
+  return tstNum > 0 && !Number.isNaN(tstNum) && val instanceof Date;
 }
 
+/**
+ * daysInMonth
+ * @param Y number - Year
+ * @param M number - Month
+ * @returns number - Number of days in month
+ */
 export function daysInMonth(Y: number, M: number) { return new Date(Y, M, 0).getDate(); }
 
+/**
+ * getDateArray
+ * @param D Date - Date to convert to array
+ * @returns number[] - Array of [year, month, day]
+ */
 export function getDateArray(D: Date) { return [D.getFullYear(), D.getMonth() + 1, D.getDate()]; }
 
+/**
+ * formatMonthYear
+ * @param D Date - Date to format
+ * @returns string - Formatted date string (e.g. 'January 2021')
+ */
 export function formatMonthYear(D: Date) {
   return `${MONTHS[D.getMonth()]} ${D.getFullYear()}`;
 }
 
+/**
+ * parseDateString
+ * @param str string - Date string (e.g. '2021-01-01')
+ * @returns Date - Date object from string
+ */
 export function parseDateString(str: string) {
   const [Y, M, D] = str.split('-').map((x) => Number.parseInt(x, 10));
   return new Date(Y, M - 1, D);
 }
 
+/**
+ * validateInputFormat
+ * @param format string - Format to validate (e.g. 'dd/mm/yyyy')
+ * @returns boolean - True if format exists in predefined INPUT_FORMATS array
+ */
 export function validateInputFormat(format: string) {
   return INPUT_FORMATS.includes(format.toLowerCase());
 }
 
+/**
+ * formatDateForInput
+ * @param date Date - Date Object to format
+ * @param inputFormat string - (e.g. 'dd/mm/yyyy')
+ * @returns string - formatted date string (e.g. '01/01/2021')
+ */
 export function formatDateForInput(date: Date, inputFormat: string) {
   const format = inputFormat && typeof inputFormat === 'string'
     ? inputFormat.toLowerCase()
@@ -74,6 +124,17 @@ export function formatDateForInput(date: Date, inputFormat: string) {
   return format.replace(fD, D as string).replace(fM, M).replace(fY, year as any);
 }
 
+/**
+ * validateSelectedDay - Ensures selected day is not greater than the number of days in the month
+ * 
+ * Set the selected day to the maximum number of days in the new month only
+ * if that new month has fewer days than the selected day
+ * 
+ * @param selectedDay number - selected day
+ * @param currentMonth number - current month
+ * @param currentYear number - current year
+ * @returns number - largest of selected day or number of days in current month
+ */
 export function validateSelectedDay(selectedDay: number, currentMonth: number, currentYear: number): number {
   const daysInMonth = (month: number, year: number): number => {
     switch (month) {
--- test/dateHelpers.test.ts
+++ test/dateHelpers.test.ts
@@ -1,87 +1,134 @@
 import { describe, expect, test } from '@jest/globals';
+
 import {
+  isLeapYear,
+  getOrdinal,
   isDateValid,
   daysInMonth,
   getDateArray,
   parseDateString,
+  formatDateForInput,
+  formatMonthYear,
+  validateSelectedDay,
 } from '../src/helpers/dateHelpers';
+
+import pickerConstants from '../src/constants/constants';
+
+import { PickerConstantsInterface } from '../src/models/interfaces';
+
+const {
+  INPUT_FORMATS,
+  MONTHS
+} = pickerConstants as PickerConstantsInterface;
+
+describe('isLeapYear', () => {
+  test('returns true for leap year', () => {
+    expect(isLeapYear(2020)).toBe(true);
+  });
+
+  test('returns false for non-leap year', () => {
+    expect(isLeapYear(2021)).toBe(false);
+  });
+});
+
+describe('getOrdinal', () => {
+  test('returns number with correct ordinal as string', () => {
+    const ordinalTstArr: string[] = [
+      '1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th',
+      '11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th',
+      '20th', '21st', '22nd', '23rd', '24th', '25th', '26th', '27th', '28th',
+      '29th', '30th', '31st'
+    ];
+    ordinalTstArr.forEach((str, index) => {
+      expect(getOrdinal(index + 1)).toBe(str);
+    });
+  });
+});
 
 describe('isDateValid', () => {
   test('returns true for valid date', () => {
     expect(isDateValid(new Date())).toBe(true);
+    expect(isDateValid(new Date(2023, 1, 1))).toBe(true);
+    expect(isDateValid(new Date('2023-01-01'))).toBe(true);
   });
 
   test('returns false for invalid date', () => {
     expect(isDateValid(new Date('foo'))).toBe(false);
+    expect(isDateValid(new Date(1959, 1, 1))).toBe(false);
+
   });
 });
 
 describe('daysInMonth', () => {
-  test('returns correct number of days in month', () => {
-    expect(daysInMonth(2021, 2)).toBe(28);
+  test('returns correct number of days in month on a normal year', () => {
+    const listOfDays: number[] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    listOfDays.forEach((day, index) => {
+      expect(daysInMonth(2021, index + 1)).toBe(day);
+    });
   });
 
-  test('returns correct number of days in leap year', () => {
+  test('returns correct number of days in february on a leap year', () => {
     expect(daysInMonth(2020, 2)).toBe(29);
-  });
-
-  test('returns correct number of days in month', () => {
-    expect(daysInMonth(2021, 3)).toBe(31);
-  });
-
-  test('returns correct number of days in month', () => {
-    expect(daysInMonth(2021, 4)).toBe(30);
-  });
-
-  test('returns correct number of days in month', () => {
-    expect(daysInMonth(2021, 5)).toBe(31);
-  });
-
-  test('returns correct number of days in month', () => {
-    expect(daysInMonth(2021, 6)).toBe(30);
-  });
-
-  test('returns correct number of days in month', () => {
-    expect(daysInMonth(2021, 7)).toBe(31);
-  });
-
-  test('returns correct number of days in month', () => {
-    expect(daysInMonth(2021, 8)).toBe(31);
-  });
-
-  test('returns correct number of days in month', () => {
-    expect(daysInMonth(2021, 9)).toBe(30);
-  });
-
-  test('returns correct number of days in month', () => {
-    expect(daysInMonth(2021, 10)).toBe(31);
-  });
-
-  test('returns correct number of days in month', () => {
-    expect(daysInMonth(2021, 11)).toBe(30);
-  });
-
-  test('returns correct number of days in month', () => {
-    expect(daysInMonth(2021, 12)).toBe(31);
   });
 });
 
 describe('getDateArray', () => {
-  test('returns correct array', () => {
-    expect(getDateArray(new Date(2021, 0, 1))).toEqual([2021, 1, 1]);
-  });
-
-  test('returns correct date', () => {
-    expect(getDateArray(new Date(2021, 1, 2))).toEqual([2021, 2, 2]);
+  test('returns date as array correctly', () => {
+    for (let i = 0; i < 12; i++) {
+      expect(getDateArray(new Date(2023, i, 1))).toEqual([2023, i + 1, 1]);
+    }
   });
 });
 
 describe('parseDateString', () => {
-  test('returns correct date', () => {
-    expect(parseDateString('2021-01-01')).toEqual(new Date(2021, 0, 1));
+  test('parses date from string', () => {
+    for (let i = 0; i < 9; i++) {
+      expect(parseDateString(`2023-0${i + 1}-01`)).toEqual(new Date(2023, i, 1));
+    }
+    for (let i = 9; i < 12; i++) {
+      expect(parseDateString(`2023-${i + 1}-01`)).toEqual(new Date(2023, i, 1));
+    }
   });
+});
 
-  test('returns correct date', () => {
-    expect(parseDateString('2021-02-01')).toEqual(new Date(2021, 1, 1));
+describe('formatDateForInput', () => {
+  test('returns correct formatted date from date object and format option', () => {
+    const date: Date = new Date(2023, 0, 9);
+    const tstFormatArr: string[] = [
+      '09012023',
+      '09/01/2023',
+      '01/09/2023',
+      '09-01-2023',
+      '01-09-2023',
+      'January 9th, 2023',
+      'January 9th 2023',
+      'Jan 9th 2023',
+      'Jan 9th, 2023',
+    ];
+    INPUT_FORMATS.forEach((format, index) => {
+      expect(formatDateForInput(date, format)).toEqual(tstFormatArr[index]);
+    });
+  });
+});
+
+describe('formatMonthYear', () => {
+  test('returns correct formatted month and year from date object', () => {
+    for (let i = 0; i < 12; i++) {
+      expect(formatMonthYear(new Date(2023, i, 1))).toEqual(`${MONTHS[i]} 2023`);
+    }
+  });
+});
+
+describe('validateSelectedDay', () => {
+  test('Set the selected day to the maximum number of days in the new month only if that new month has fewer days than the selected day', () => {
+    // Leap year
+    expect(validateSelectedDay(31, 2, 2020)).toBe(29);
+
+    // Non-leap year
+    expect(validateSelectedDay(31, 2, 2023)).toBe(28);
+    expect(validateSelectedDay(31, 4, 2023)).toBe(30);
+    expect(validateSelectedDay(31, 6, 2023)).toBe(30);
+    expect(validateSelectedDay(31, 9, 2023)).toBe(30);
+    expect(validateSelectedDay(31, 11, 2023)).toBe(30);
   });
 });
